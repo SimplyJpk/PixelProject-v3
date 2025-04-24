5e9b705eaeec2b7ff8d705e34fe0ae86da7a46b8
James Kellie <james_kellie@live.com>
2025-04-24 15:01:51 +1000

    :art: Refactor WorldDataHandler formatting for improved readability and consistency in code style

--- PixelProject/Scripts/World/Pixels/WorldDataHandler.cpp
+++ PixelProject/Scripts/World/Pixels/WorldDataHandler.cpp
@@ -7,7 +7,7 @@
 #include "PixelTypes.h"
 #include "World/PixelMask.h"
 
-WorldDataHandler* WorldDataHandler::GetInstance()
+WorldDataHandler *WorldDataHandler::GetInstance()
 {
 	static WorldDataHandler instance;
 	return &instance;
@@ -25,56 +25,62 @@ WorldDataHandler::WorldDataHandler()
 	};
 
 	int index = 0;
-	for (auto& pixel : _pixel_types) {
+	for (auto &pixel : _pixel_types)
+	{
 		// pixel.pixel_index = index++; // Handled by constructor of BasePixel now
-		for (auto i = 0; i < pixel.colour_count; ++i) {
+		for (auto i = 0; i < pixel.colour_count; ++i)
+		{
 			if (_pixel_colour_map.contains(pixel.type_colours[i]))
 				DEBUG_WARNING_LOG("[WorldDataHandler] Duplicate pixel colour {} found for pixel type {}.",
-					pixel.type_colours[i],
-					static_cast<uint32_t>(pixel.pixel_type));
+								  pixel.type_colours[i],
+								  static_cast<uint32_t>(pixel.pixel_type));
 			_pixel_colour_map[pixel.type_colours[i]] = &pixel;
 		}
 		_pixel_type_map[pixel.pixel_type] = &pixel;
 	}
 }
 
-Uint32 WorldDataHandler::GetPixelColour(const Pixel::PixelType& pixel_type) const
+Uint32 WorldDataHandler::GetPixelColour(const Pixel::PixelType &pixel_type) const
 {
 	return _pixel_type_map.at(pixel_type)->GetRandomColour();
 }
 
-const std::array<uint8_t, 8>& WorldDataHandler::GetRandomPixelUpdateOrder(Pixel::PixelType pixel_type) const
+const std::array<uint8_t, 8> &WorldDataHandler::GetRandomPixelUpdateOrder(Pixel::PixelType pixel_type) const
 {
 	return _pixel_type_map.at(pixel_type)->GetPixelUpdateOrder();
 }
 
-BasePixel* WorldDataHandler::GetPixelFromIndex(const uint8_t index)
+BasePixel *WorldDataHandler::GetPixelFromIndex(const uint8_t index)
 {
 	return &_pixel_types[index];
 }
 
-BasePixel* WorldDataHandler::GetPixelFromType(const Pixel::PixelType& pixel_type) const
+BasePixel *WorldDataHandler::GetPixelFromType(const Pixel::PixelType &pixel_type) const
 {
 	return _pixel_type_map.at(pixel_type);
 }
 
-void WorldDataHandler::SetUniformData(Shader* shader)
+void WorldDataHandler::SetUniformData(Shader *shader)
 {
 	shader->UseProgram();
 	const auto programID = shader->GetProgramID();
 	GLuint loc;
-	for (int i = 0; i < TYPE_COUNT; i++) {
+	for (int i = 0; i < TYPE_COUNT; i++)
+	{
 		auto pixel = _pixel_types[i];
 		std::string locationString = std::format("u_Pixels[{}].colour_count", static_cast<int>(pixel.pixel_index));
 		loc = shader->GetUniformLocation(locationString);
 		const auto colourCount = static_cast<GLuint>(pixel.colour_count);
 		glProgramUniform1ui(programID, loc, colourCount);
-		float pixelColour[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
-		for (short colourIndex = 0; colourIndex < pixel.colour_count; colourIndex++) {
-			if (colourIndex < pixel.colour_count) {
-				const auto colourLoc = shader->GetUniformLocation(std::format("u_Pixels[{}].colours[{}]",
-					pixel.pixel_index,
-					static_cast<int>(colourIndex)));
+		float pixelColour[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+		for (short colourIndex = 0; colourIndex < pixel.colour_count; colourIndex++)
+		{
+			if (colourIndex < pixel.colour_count)
+			{
+				const auto colourLoc = shader->GetUniformLocation(
+					std::format("u_Pixels[{}].colours[{}]",
+								pixel.pixel_index,
+								static_cast<int>(colourIndex)));
 				pixel.GetIndexAs4FColour(colourIndex, pixelColour);
 				glProgramUniform4fv(programID, colourLoc, 4, pixelColour);
 			}
@@ -100,5 +106,4 @@ void WorldDataHandler::SetUniformData(Shader* shader)
 	glProgramUniform1ui(programID, loc, PixelMask::Behaviour::DEPTH);
 	loc = shader->GetUniformLocation("u_PixelBitOffset.light");
 	glProgramUniform1ui(programID, loc, PixelMask::Light::DEPTH);
-
 }
--- PixelProject/Scripts/World/Pixels/WorldDataHandler.h
+++ PixelProject/Scripts/World/Pixels/WorldDataHandler.h
@@ -7,36 +7,36 @@
 
 class WorldDataHandler
 {
- public:
-	static WorldDataHandler* GetInstance();
+public:
+	static WorldDataHandler *GetInstance();
 
 	// TODO : (James) Utility functions
 	// std::string GetPixelName(const Pixels::PixelType& pixel_type) const;
 	// const BasePixel& GetPixelFromColour(const Uint32& colour) const;
 
-	BasePixel* GetPixelFromIndex(const uint8_t index);
-	BasePixel* GetPixelFromType(const Pixel::PixelType& pixel_type) const;
+	BasePixel *GetPixelFromIndex(const uint8_t index);
+	BasePixel *GetPixelFromType(const Pixel::PixelType &pixel_type) const;
 
-	const std::array<uint8_t, 8>& GetRandomPixelUpdateOrder(Pixel::PixelType pixel_type) const;
-	Uint32 GetPixelColour(const Pixel::PixelType& pixel_type) const;
+	const std::array<uint8_t, 8> &GetRandomPixelUpdateOrder(Pixel::PixelType pixel_type) const;
+	Uint32 GetPixelColour(const Pixel::PixelType &pixel_type) const;
 
-	void SetUniformData(Shader* shader);
+	void SetUniformData(Shader *shader);
 
-	WorldDataHandler(const WorldDataHandler&) = delete;
-	WorldDataHandler(WorldDataHandler&&) = delete;
-	void operator=(const WorldDataHandler&) = delete;
-	void operator=(WorldDataHandler&&) = delete;
+	WorldDataHandler(const WorldDataHandler &) = delete;
+	WorldDataHandler(WorldDataHandler &&) = delete;
+	void operator=(const WorldDataHandler &) = delete;
+	void operator=(WorldDataHandler &&) = delete;
 
- protected:
+protected:
 	WorldDataHandler();
 
- private:
+private:
 	~WorldDataHandler() = default;
 
 	// Main array holding pixel types in contiguous memory
 	std::array<BasePixel, Pixel::TYPE_COUNT> _pixel_types;
 
 	// utility structures for fast lookup and iteration
-	std::unordered_map<Uint32, BasePixel*> _pixel_colour_map;
-	std::unordered_map<Pixel::PixelType, BasePixel*> _pixel_type_map;
+	std::unordered_map<Uint32, BasePixel *> _pixel_colour_map;
+	std::unordered_map<Pixel::PixelType, BasePixel *> _pixel_type_map;
 };
